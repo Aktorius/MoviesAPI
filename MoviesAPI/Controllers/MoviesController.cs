eb3ff790dc04b4d6ec329c5b3199c0b06ae9ae30
Tarik Miri <tmiri@euroffice.co.uk>
2017-04-20 16:35:26 +0100

    Create MoviesController with basic CRUD actions

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/movies")]
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            return Ok(new List<MovieModel>());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
+        {
+            MovieModel movie = null;
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMovie(MovieModel movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok();
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int id)
+        {
+            MovieModel movie = null;
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok();
+        }
+    }
+}
