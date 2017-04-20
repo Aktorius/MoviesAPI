4b1d459322a06f545aa8d3169a5c891069916257
Tarik Miri <tmiri@euroffice.co.uk>
2017-04-20 16:16:57 +0100

    Create very basic Movie Model

---
+++ MoviesAPI/Models/MovieModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesAPI.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Synopsis { get; set; }
+    }
+}
